96ccd6c6399bdfb93e16ac3ee52e647242890a77
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-05-21 21:36:52 -0700

    routes for database and api call

---
+++ client/src/utils/API.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+require('dotenv').config()
+
+export default {
+    //get all books
+    getBooks:function () {
+        return axios.get('/api/books');
+    },
+    //get one book with id
+    getOneBook: function(id) {
+        return axios.get('/api/books/' + id);
+    },
+    deleteBook: function(id) {
+        return axios.delete('/api/books/' + id);
+    },
+    saveBook: function(bookData) {
+        return axios.post('/api/books', bookData);
+    },
+    googleBook: function(query) {
+        const key = process.env.GbookAPIkey;
+        const url = `https://www.googleapis.com/books/v1/volumes?`;
+        
+        return axios.get(`${url}${query}&key=${key}`);
+    }
+
+
+
+}
