3a6e05ee43b9f1e73297b925613d43947fb79898
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-05-23 22:27:50 -0700

    start seach js

---
+++ client/src/pages/Search.js
@@ -0,0 +1,35 @@
+import React, { Component, useState, useEffect } from 'react'
+import API from '../utils/API';
+
+//components
+import { ViewBtn, SaveBtn } from '../components/Buttons';
+import { Col, Row, Container } from '../components/Grid';
+import { Input, TextArea, FormBtn } from '../components/Form';
+import Wrapper from '../components/Wrapper';
+import Nav from '../components/Nav';
+
+
+
+class Search extends Component {
+    state = {}
+
+    render() {
+        <Wrapper>
+            <Nav/>
+            <Container>
+                <p>title</p>
+            </Container>
+            <Container>
+                
+            </Container>
+        </Wrapper>
+        //title
+        //container
+        //search bar
+        //container
+        //book card
+        //results list
+            //view button
+            //save button
+    }
+}
