0d68e299c2c08b8949b336a5ea3b9f15b63e8c9b
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-05-27 18:05:12 -0700

    modify props

--- client/src/components/BookCard/index.js
+++ client/src/components/BookCard/index.js
@@ -1,42 +1,25 @@
-import React, { Component } from 'react';
-import API from '../../utils/API';
-import Card from '../Card';
-import { Row, Col, Container } from '../Grid';
-import Wrapper from '../Wrapper';
+import React from 'react';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
-export default class BookCard extends Component {
-    state = {
-        loading: true,
-        books: null
-    }
 
-    async componentDidMount() {
-        const { data } = await API.getBooks();
-        this.setState({ books: data.results, loading: false })
 
-        console.log(data.results);
-    }
-
-    savedBooks(props) {
-        if(this.state.loading || !this.state.books) {
-            return (
-                <div>Loading...</div>
-            )
-        }
-
-        return(
-            <Wrapper>
-                {this.state.books
-                .map((books) => (
-                    <Card
-                    key= {books.isbn}
-                    text1= {books.title}
-                    image= {books.thumbnail}
-                    text2= {books.authors.join(' , ')}
-                    text3= {books.description}
-                    />
-                ))}
-            </Wrapper>
-        )
-    }
+export default function BookCard(props) {
+   return (
+            <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={props.image} />
+            <Card.Body>
+              <Card.Title>{props.title}</Card.Title>
+              <Card.Text>
+                {props.authors}
+                <br/>
+                {props.description}
+              </Card.Text>
+              <Button variant="primary">{props.btn1}props1</Button>
+              <Button variant="primary">{props.btn2}props 2</Button>
+            </Card.Body>
+          </Card>
+       
+        
+  )  
 }
