9a4feabdefa4d1c94d0a282eba5778b3b6a9e92e
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-05-31 17:43:09 -0700

    add page routes to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Search from './pages/Search'
+import Nav from "./components/Nav";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+
 
 function App() {
   return (
-    <>
-    <Search/>
-    </>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/search">
+            <Search />
+          </Route>
+          <Route exact path="/saved">
+            <Saved />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
