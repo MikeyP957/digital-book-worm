2ae0c272294a55d577395dcfbc6625a2f7a93c1b
mikeyp957 <peraram2@gmail.com>
2021-06-05 15:36:54 -0700

    add style sheet, refractor

--- client/src/components/BookCard/index.js
+++ client/src/components/BookCard/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import '../../App.css'
 
 
 
 export default function BookCard(props) {
    return (
-            <Card style={{ width: '18rem' }}>
+            <Card className="book-card" style={{ width: '18rem' }}>
             <Card.Img variant="top" src={props.image || "https://www.fillmurray.com/640/360" } />
             <Card.Body>
               <Card.Title>{props.title}</Card.Title>
