9c942cc85ae979b09a6b45dceda1b6a4b1c50c42
mikeyp957 <peraram2@gmail.com>
2021-06-05 15:35:48 -0700

    refractor

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import API from '../utils/API'
 import SaveBookCard from '../components/SaveBookCard'
+import Header from '../components/Header'
 import Button from 'react-bootstrap/Button'
+import '../App.css'
 
 export default function Search() {
     const [data, setData] = useState([]);
     const [query, setQuery] = useState('');
    
-
-
     function searchHandler(e) {
         //the api takes words with the primary letter in each word capitalized
         const mySentence = query;
@@ -17,21 +17,16 @@ export default function Search() {
         const finalSentence = mySentence.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
         console.log(finalSentence)
 
-
         API.googleBook(finalSentence)
             .then((response) => setData(response.data.items))
 
         console.log(data)
     }
+ 
 
-    
-
-
-  
-    (data[0]) ? console.log('data[0] is something', data[0]): console.log('its nothing...')
     return(         
         <div>
-            
+            <Header />
             <div>
 
                 <input type= "text" onChange={event => setQuery(event.target.value)}/>
@@ -41,12 +36,12 @@ export default function Search() {
                 <div className='container row'>
                     {data.map((book) => (
                         <div>
-                        <SaveBookCard 
-                            key= {book.id}
-                            title= {book.volumeInfo.title}
-                            
+                        <SaveBookCard
+                            key={book.id}
                             authors = {book.volumeInfo.authors}
+                            title={book.volumeInfo.title}
                             description = {book.volumeInfo.description}
+                            image={book.volumeInfo.imageLinks.thumbnail}
                         />
 
                         </div>
@@ -57,9 +52,5 @@ export default function Search() {
             )}
         </div>               
     )
-
 }
 
-// image={book.volumeInfo.imageLinks.thumbnail}
-
-// }
