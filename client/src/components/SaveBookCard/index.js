bcf89b8b9fe8025598f084a569a65ece15dc57ea
mikeyp957 <peraram2@gmail.com>
2021-06-05 15:36:31 -0700

    save book is working, image loads to saved page

--- client/src/components/SaveBookCard/index.js
+++ client/src/components/SaveBookCard/index.js
@@ -3,20 +3,21 @@ import Wrapper from '../Wrapper'
 import API from '../../utils/API';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import '../../App.css'
 
 
 export default function SaveSearchedBook(props) {
     const [savedBook, setSaved] = useState(false);
 
-    function handleSave(book) {
-        console.log(book)
-        API.saveBook(book)
+    function handleSave(aBook) {
+        console.log(aBook)
+        API.saveBook(aBook)
             .then(setSaved(true))
             .catch(err => console.log(err))
     }
 
     return(     
-        <Card style={{ width: '18rem' }}>
+        <Card className="book-card" style={{ width: '18rem' }}>
             <Card.Img variant="top" src={props.image} />
             <Card.Body>
               <Card.Title>{props.title}</Card.Title>
@@ -27,7 +28,9 @@ export default function SaveSearchedBook(props) {
              <Button onClick= {() => !savedBook && handleSave({
                     title: props.title,
                     authors: props.authors,
-                    description: props.description
+                    description: props.description,
+                    imageLink: props.image
+                    
                     })
             } >Save book to Library</Button>
             </Card.Body>
