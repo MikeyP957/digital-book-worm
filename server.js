a68e2b14c971756c09e7174268d53789d6063e94
mikeyp957 <peraram2@gmail.com>
2021-06-24 22:52:59 -0700

    trying to fix routing

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require('path')
 
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'))
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
